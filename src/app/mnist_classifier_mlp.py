e4e6c7bbf857b21761ac269cc4be774195f46eb8
Frank Lehner <frank-lehner71@t-online.de>
2023-11-09 14:08:33 +0100

    app/mnist_classifier: Application for mnist mlp classifier

---
+++ src/app/mnist_classifier_mlp.py
@@ -0,0 +1,71 @@
+"""Application for mnist classifier mlp
+"""
+from typing import List, Tuple
+
+from src.domain.mnist_mlp import MnistClassifier
+from src.infra.datasets import Mnist as MnistDatasets
+
+
+def load_mnist() -> MnistDatasets:
+    """load data"""
+    mnist = MnistDatasets()
+
+    return mnist
+
+
+def generate_layers(
+    count: int, layer: str, units: int, num_labels: int
+) -> List[Tuple[str, int]]:
+    """generate layers"""
+    layers = [(layer, units) for _ in range(count - 1)]
+    layers.append((layer, num_labels))
+
+    return layers
+
+
+def generate_activations(count: int, activation: str, last: str) -> List[str]:
+    """generate activations"""
+    activations = [activation for _ in range(count - 1)]
+    activations.append(last)
+
+    return activations
+
+
+def generate_dropouts(count: int, rate: float) -> List[float]:
+    """generate dropouts"""
+    dropouts = [rate for _ in range(count)]
+
+    return dropouts
+
+
+def run(  # pylint: disable=too-many-arguments
+    mnist: MnistDatasets,
+    layers: List[Tuple[str, int]],
+    activations: List[str],
+    dropouts: List[float],
+    model_path: str,
+    batchsize: int,
+    epochs: int,
+):
+    """runner method"""
+    model = MnistClassifier(
+        input_dim=mnist.input_size,
+        layers=layers,
+        activations=activations,
+        dropout=dropouts,
+        model_path=model_path,
+        batch_size=batchsize,
+    )
+    model.compile(
+        loss="categorical_crossentropy",
+        optimizer="adam",
+        metrics=["accuracy"],
+    )
+    model.fit(mnist.x_train, mnist.y_train, epochs=epochs)
+    accuracy = model.evaluate(
+        test_data=mnist.x_test,
+        test_targets=mnist.y_test,
+    )
+    model.save()
+
+    print(f"\n\nTest accuracy: {accuracy}")
