0359386e7f231523514cd0e7d802a920117606ce
Frank Lehner <frank-lehner71@t-online.de>
2023-11-10 11:19:40 +0100

    scripts/mnist_mlp_predict: Add script to predict mnist images

---
+++ scripts/mnist_mlp_predict.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""This script ...
+"""
+import os
+import logging
+import click
+import numpy as np
+
+import src.app.mnist_classifier_mlp as mlp_cls
+
+
+_log = logging.getLogger(__name__)
+
+
+BASE_PATH = "data/"
+
+
+@click.command()
+@click.option(
+    "--model-name",
+    "-m",
+    default="mlp-mnist.keras",
+    help="Filepath to the model",
+)
+def cli(model_name: str):
+    """Client
+    """
+    filename = os.path.join(BASE_PATH, model_name)
+    mnist = mlp_cls.load_mnist()
+    indexes = np.random.randint(0, mnist.x_test.shape[0], size=10)
+    test_images = mnist.x_test[indexes]
+    images = test_images.reshape(-1, 28, 28)
+    labels = mnist.y_test[indexes]
+    model = mlp_cls.load_model(filepath=filename)
+    for i in range(len(indexes)):
+        print(f"Real label: {np.argmax(labels[i])}")
+        result = model.predict(image=test_images[i].reshape(-1, test_images[i].shape[0]))
+        print(f"Predicted: {np.argmax(result)}")
+
+
+if __name__ == "__main__":
+    cli()  # pylint: disable=no-value-for-parameter
