db4f8fe45cf8effba0f961ac1e8819ae9484ded2
Frank Lehner <frank-lehner71@t-online.de>
2023-11-17 15:39:01 +0100

    infra/datasets: Add reader for cifar10 and cifar100

--- src/infra/datasets.py
+++ src/infra/datasets.py
@@ -2,7 +2,7 @@
 """
 from dataclasses import dataclass
 
-from keras.datasets import mnist
+from keras.datasets import mnist, cifar10, cifar100
 
 from src.domain.models.datasets import Mnist
 
@@ -17,4 +17,36 @@ class MnistReader:
         """load train and test data from the keras dataset"""
         (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-        return Mnist(x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test)
+        return DataSet(x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test)
+
+
+@dataclass
+class Cifar10Reader:
+    """class of the cifar10 data from keras"""
+
+    def load(self) -> DataSet:
+        """load train and test data from cifar10 dataset"""
+        (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+        return DataSet(
+            x_train=x_train,
+            x_test=x_test,
+            y_train=y_train,
+            y_test=y_test,
+        )
+
+
+@dataclass
+class Cifar100Reader:
+    """class of the cifar 100 dataset from keras"""
+
+    def load(self) -> DataSet:
+        """load cifar100 data"""
+        (x_train, y_train), (x_test, y_test) = cifar100.load_data()
+
+        return DataSet(
+            x_train=x_train,
+            y_train=y_train,
+            x_test=x_test,
+            y_test=y_test,
+        )
